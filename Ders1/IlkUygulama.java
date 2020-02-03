0dda5b7726af0da0ba19e11c335384508b4485d9
Celal Ceken <celalceken@gmail.com>
2020-02-03 23:44:23 +0300

    İlk Uygulama

--- Ders1/IlkUygulama.java
+++ Ders1/IlkUygulama.java
@@ -1,6 +1,13 @@
+
+/**
+* IlkUygulama.java olarak kaydedilmeli.
+*
+*/
+
 public class IlkUygulama 
 {
     public static void main(String[] s) {
+        // Merhaba Dünya ifadesi çıkış konsoluna (ekrana) yönlendiriliyor   
         System.out.println("Merhaba Dünya");
     }
 }
