059bab0dfb43837c3449affa7e44c6d00eedee83
Celal Ceken <celalceken@users.noreply.github.com>
2021-03-02 17:21:03 +0300

    Arayüzler

--- Ders2/3Interface/VeritabaniIslemleri.java
+++ Ders2/3Interface/VeritabaniIslemleri.java
@@ -1,9 +1,14 @@
 /*
-*  Program to interface, not implementation
-* Modülü kodlarken arayüzler yerine gerçeklemelerin kullanılması durumunda;
-* i )değişiklik (yeni özellik ekleme) istendiğinde istemci kodu değiştirmek gerekir
-* i) modüldeki değişiklik istemciyi etkileyebilir
-* ii)test yazmak zorlaşır
+* "Program to interface, not implementation"
+* Bu modül (istemci kod) veritabanı işlemlerini gerçekleştirmek üzere PostgreSQL, MySQL vb.
+* gerçeklemeleri kullanmak yerine "VeritabaniSurucu" arayüzünü kullanıyor.
+* böylece veritabanı işlemlerinin nasıl yapılacağını tanımlayan gerçeklemeler yerine
+* ne yapılması gerektiğini söyleyen arayüzü kullanarak kodlama yapılıyor.
+* Bu sayede:
+* i )değişiklik  istendiğinde istemci kod değiştirilmiyor
+* ii) gerçekleme kısmında yapılacak değişiklik istemci kodu etkilemiyor.
+* iii) gerçeklemeler henüz ortada yokken arayüzler istemci kod içerisinde kullanılabilir.
+*       Takım çalışması, yeni özelliklerin kolayca eklenebilmesi.
 *
 * */
 package cc.ders2.arayuz;
